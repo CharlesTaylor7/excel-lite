ea1232d4cecc52131fd37ae3af06d4208c5ac186
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-03-25 21:33:27 -0400

    More tests

--- test/ExcelSpec.hs
+++ test/ExcelSpec.hs
@@ -8,27 +8,52 @@ test = it
 
 spec = do
   describe "Excel" $ do
-    test "literals: readCell gets the literal value set" $ do
-      let
-        id = CellId 0
-        sheet = emptySheet & setCell id (Lit 2)
-        value = readCell id sheet
-      value `shouldBe` pure 2
+    describe "readCell" $ do
+      test "reports EmptyCell when cell is not initialized" $ do
+        readCell (CellId 0) emptySheet `shouldBe` Left EmptyCell
 
-    test "refs: readCell reports no ref when ref doesnt exist " $ do
-      let
-        id = CellId 0
-        sheet = emptySheet & setCell id (Ref . CellId $ 2)
-        value = readCell id sheet
-      value `shouldBe` Left NonexistentRef
+      test "reports EmptyCell when cell is initialized, but empty" $ do
+        let
+          id = CellId 7
+          sheet = emptySheet & _Excel . at id ?~ emptyCell
+        readCell id sheet `shouldBe` Left EmptyCell
 
-    test "refs: readCell reads ref when set" $ do
-      let
-        id1 = CellId 0
-        id2 = CellId 2
-        sheet = emptySheet
-          & setCell id1 (Ref . CellId $ 2)
-          & setCell id2 (Lit 5)
-        value = readCell id1 sheet
+      describe "literals" $ do
+        test "gets the literal value set" $ do
+          let
+            id = CellId 0
+            sheet = emptySheet & setCell id (Lit 2)
+            value = readCell id sheet
+          value `shouldBe` pure 2
+      describe "refs" $ do
+        test "reads ref when set" $ do
+          let
+            id1 = CellId 0
+            id2 = CellId 2
+            sheet = emptySheet
+              & setCell id1 (Ref . CellId $ 2)
+              & setCell id2 (Lit 5)
+            value = readCell id1 sheet
 
-      value `shouldBe` pure 5
+          value `shouldBe` pure 5
+
+        test "reports no ref when ref doesnt exist " $ do
+          let
+            id = CellId 0
+            sheet = emptySheet & setCell id (Ref . CellId $ 2)
+            value = readCell id sheet
+          value `shouldBe` Left NonexistentRef
+
+        test "reports cyclic ref when ref refers to itself " $ do
+          let
+            id = CellId 0
+            sheet = emptySheet & setCell id (Ref id)
+            value = readCell id sheet
+          value `shouldBe` Left CyclicReference
+
+        test "reports cyclic ref when ref refers to itself " $ do
+          let
+            id = CellId 0
+            sheet = emptySheet & setCell id (Ref id)
+            value = readCell id sheet
+          value `shouldBe` Left CyclicReference
