eef1cb44ecb14b63d51ce0d91c38a252533213db
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-03-28 13:38:41 -0400

    Langauge def

--- src/Excel/Parser.hs
+++ src/Excel/Parser.hs
@@ -5,5 +5,20 @@ module Excel.Parser
 import Internal.Imports
 import Excel.Types
 
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Expr
+import Text.ParserCombinators.Parsec.Language
+import qualified Text.ParserCombinators.Parsec.Token as Token
+
 parseInput :: String -> Maybe Write
 parseInput = undefined
+
+languageDef = emptyDef
+  { Token.commentStart    = "/*"
+  , Token.commentEnd      = "*/"
+  , Token.commentLine     = "//"
+  , Token.identStart      = letter
+  , Token.identLetter     = alphaNum
+  , Token.reservedNames   = []
+  , Token.reservedOpNames = ["+", "*", "="]
+  }
