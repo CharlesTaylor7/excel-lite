77cfe462c2fea0ee96a050f3a4d82486329549c6
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-03-28 13:12:57 -0400

    Remove transformers dependency

--- src/Internal/Imports.hs
+++ src/Internal/Imports.hs
@@ -11,7 +11,6 @@ module Internal.Imports
   , module RIO
   , module Control.Monad.State.Strict
   , module Control.Monad.Except
-  , module Control.Monad.Trans.Maybe
   , identity
   ) where
 
@@ -34,9 +33,6 @@ import Control.Lens
 -- mtl
 import Control.Monad.State.Strict
 import Control.Monad.Except
-
--- transformers
-import Control.Monad.Trans.Maybe
 
 
 identity :: a -> a
