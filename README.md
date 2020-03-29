facbba5db91b25a5292d93a1f08424a602a23699
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-03-28 20:56:39 -0400

    Implement cell labels

--- README.md
+++ README.md
@@ -3,10 +3,9 @@ A boring console app that does simple excel like things.
 I'm using this as opportunity to familiarize myself with lens & parsec.
 
 # To Do
+- Label Cells
 - Don't display redundant parentheses
-- Vertically align expression cells with output cells
 - Implement quit command
 - Evaluate expressions at prompt
 - Implement clear cell command
-- Label Cells
 - Make sheet 2d
--- src/Excel/Pretty.hs
+++ src/Excel/Pretty.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Excel.Pretty where
 
 import Internal.Imports
 import Excel.Types
+import Excel.Operations
 
 import Text.Parsec (ParseError(..))
 
@@ -15,19 +17,45 @@ class Pretty a where
 instance Pretty ParseError where
   pretty = show
 
-instance Pretty a => Pretty (Sheet a) where
+instance Pretty SheetCells where
   pretty sheet =
     let
       cellIds = enumFromTo (CellId 0) $ sheet ^. sheet_maxId
-      getCell id = sheet ^? sheet_cells . ix id
+      values = evalSheet sheet
+      getCell sheet id = sheet ^? sheet_cells . ix id
       anyCells = has (sheet_cells . folded) sheet
+      prettify sheet = pretty . getCell sheet
+      formatColumn = padToLongest . \id ->
+        [ "$" <> show (id ^. _CellId)
+        , prettify sheet id
+        , prettify values id
+        ]
     in
       if anyCells
       then
         cellIds
-        & map (pretty . getCell)
-        & intercalate " | "
+        & map formatColumn
+        & transpose
+        & map (surround "| " " |" . intercalate " | ")
+        & intercalate "\n"
       else "<empty sheet>"
+
+surround :: Semigroup a => a -> a -> a -> a
+surround start end center = start <> center <> end
+
+padToLongest :: [String] -> [String]
+padToLongest texts =
+  case maximumOf (folded . to length) texts of
+    Just max -> map (padToCenter max) texts
+    Nothing -> []
+
+padToCenter :: Int -> String -> String
+padToCenter m text = padding start <> text <> padding end
+  where
+    n = m - length text
+    start = n `div` 2
+    end = (n `div` 2) + (n `mod` 2)
+    padding = flip replicate ' '
 
 instance Pretty Expr where
   pretty (Lit x) = pretty x
@@ -58,6 +86,10 @@ instance Pretty Int where
 instance (Pretty a, Pretty b, Pretty c) => Pretty (a, b, c) where
   pretty (a, b, c) = "[" <> joined <> "]"
     where joined = intercalate ", " [pretty a, pretty b, pretty c]
+
+instance (Pretty a, Pretty b) => Pretty (a, b) where
+  pretty (a, b) = "(" <> joined <> ")"
+    where joined = intercalate ", " [pretty a, pretty b]
 
 instance (Pretty a, Pretty b) => Pretty (Either a b) where
   pretty = pretty ||| pretty
