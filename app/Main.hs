f6fa16c842b554cec3f8c304b8d5b1af047ee058
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-03-31 20:41:40 -0400

    Solution

--- app/Main.hs
+++ app/Main.hs
@@ -1,47 +1,50 @@
-module Main where
+{--
+pseudo code (aka python )
+def spirals():
 
-import Excel
+  current = 1
+  increment = 0
+  yield 1
+  for i in xrange(1001):
+    increment += 2
+    for _ in xrange(4):
+      current += increment
+      yield current
+
+def spiral_sum:
+  sum = 0
+  for x in spirals():
+    sum += x
+--}
+module Main where
+import Data.IORef
+import Data.Foldable
+import Prelude
+
+modifyRead :: IORef a -> (a -> a) -> IO a
+modifyRead ref f = modifyIORef' ref f >> readIORef ref
+
+int :: Int -> Int
+int = id
 
 main :: IO ()
-main = loop
-  & flip evalStateT emptySheet
-  & runExceptT
-  >>= print ||| absurd
+main = do
+  sumRef <- newIORef $ int 0
+  incrementRef <- newIORef $ int 0
+  currentRef <- newIORef $ int 1
+  for_ [1..1001] $ do
+    increment <- modifyRead incrementRef (+ 2)
+    for_ [1..4] $ do
+      current <- modifyRead currentRef (+ increment)
+      modifyIORef' sumRef (+ current)
 
-loop :: (MonadState SheetCells m, MonadIO m, MonadError End m)
-     => m Void
-loop = do
-  sheet <- get
-  prettyPrint sheet
-  command <- promptUser
-  runCommand command
-  loop
+  sum <- readIORef sumRef
+  print sum
+{--
+newtype Prev = Prev Int
+newtype Inc = Inc Int
 
-data End = UserQuit
-  deriving Show
+next :: Prev -> Inc -> [Int]
+next (Prev p) (Inc i) = [p+i, p+2*i, p+3*i, p + 4*i]
 
-runCommand :: (MonadState SheetCells m, MonadIO m, MonadError End m)
-         => Command
-         -> m ()
-runCommand = \case
-    Eval expr -> do
-      sheet <- get
-      let val = evalExpr expr $ sheet
-      prettyPrint val
-    Edit id -> putStrLn "Not Implemented"
-    Delete id -> modify $ sheet_cells . at id .~ Nothing
-    Quit -> throwError UserQuit
-
-evalExpr :: Expr -> SheetCells -> CellValue
-evalExpr expr sheet =
-  eval expr
-  & flip evalStateT emptySheet
-  & flip runReader sheet
-
-promptUser :: MonadIO m => m Command
-promptUser = do
-  putStrLn ">"
-  line <- getLine
-  case parseInput line of
-    Left err -> prettyPrint err >> promptUser
-    Right x -> pure x
+--}
